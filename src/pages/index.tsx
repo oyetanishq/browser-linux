8924f57637837d1b4954f0f8e371bec934325399
Tanishq Singh <hi@tanishqsingh.com>
2024-12-12 03:20:16 +0530

    :tada: trying alpine emulator with `copy/v86` project

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+
+export default function Home() {
+	useEffect(function initializeEmulator() {
+		(window as any).emulator = new (window as any).V86({
+			wasm_path: "/v86.wasm",
+			screen_container: document.getElementById("screen_container"),
+			bios: {
+				url: "/bios/seabios.bin",
+			},
+			vga_bios: {
+				url: "/bios/vgabios.bin",
+			},
+			boot_order: "0x123", // Boot from CD-ROM first
+			memory_size: 512 * 1024 * 1024, // 512MB RAM
+			vga_memory_size: 64 * 1024 * 1024, // 64MB VGA RAM
+			// See more: https://github.com/copy/v86/blob/master/docs/networking.md
+			net_device: {
+				type: "virtio",
+				relay_url: "wisps://wisp.mercurywork.shop",
+			},
+			cdrom: {
+				// Source: https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86/alpine-virt-3.20.3-x86.iso
+				url: "/images/alpine-virt-3.20.3-x86.iso",
+			},
+			autostart: true,
+		});
+	}, []);
+
+	return (
+		<div>
+			<div id="screen_container">
+				<div id="screen" style={{ overflow: "hidden" }}>
+					Initializing Emulator…
+				</div>
+				<canvas style={{ display: "none" }}></canvas>
+			</div>
+		</div>
+	);
+}
